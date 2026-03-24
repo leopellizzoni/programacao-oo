7ddfb1fcec4678c3b8ae960d3250ff2b541ea8b5
leopellizzoni <lpellizzoni@ucs.br>
2026-03-24 20:36:44 -0300

    testes sobre objetos e heranças

--- aula05/ConsoleAppAula01/Program.cs
+++ aula05/ConsoleAppAula01/Program.cs
@@ -4,14 +4,29 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-    
+        Veiculo veiculoTeste = new Moto();
+        (veiculoTeste as Moto).CC = 150;
+        veiculoTeste.Placa = "AAAA222";
+        veiculoTeste.Modelo = "CG";
+        veiculoTeste.Marca = "Honda";
+        //GERA ERRO
+        //Console.WriteLine((veiculoTeste as Caminhao).PesoTotalParaTransporte);
 
+        Veiculo meuCaminhao = new Caminhao();
+        (meuCaminhao as Caminhao).PesoTotalParaTransporte = 150;
+        meuCaminhao.Placa = "AAAA2122";
+        meuCaminhao.Modelo = "XX2";
+        meuCaminhao.Marca = "Volvo";
+        Console.WriteLine((meuCaminhao as Caminhao).PesoTotalParaTransporte);
+        
+        Gerente g = new Gerente();
+        g.VeiculoDoFuncionario = new Carro();
 
-        Moto minhaMoto = new Moto();
-        minhaMoto.CC = 150;
-        minhaMoto.Placa = "AAAA222";
-        minhaMoto.Modelo = "CG";
-        minhaMoto.Marca = "Honda";
+        Motorista m = new Motorista();
+        m.VeiculoDoFuncionario = new Caminhao();
+
+        OfficeBoy boy = new OfficeBoy();
+        boy.VeiculoDoFuncionario = new Moto();
 
     }
 }
