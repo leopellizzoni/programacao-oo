a54bb8b792623aef7a304f8985fb6ea6b2fa8ba5
leopellizzoni <lpellizzoni@ucs.br>
2026-04-28 21:49:48 -0300

    feat: classe em pr de exemplo

---
+++ aula08/ConsoleAppAula01/ProblemaRestaurante/MenuPT_BR.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace ConsoleAppAula01.ProblemaRestaurante;
+
+public class MenuPT_BR : IMenu
+{
+    public string Prato0()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string Prato02()
+    {
+        throw new NotImplementedException();
+    }
+
+    public string Prato03()
+    {
+        throw new NotImplementedException();
+    }
+}
